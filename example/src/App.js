b08797d928066c85ff5cc3a10a94eb87ee62db10
Justin Sheckler <self@jaykayess.com>
2019-11-23 13:35:00 -0500

    Demo looks acceptable now

--- example/src/App.js
+++ example/src/App.js
@@ -79,7 +79,7 @@ export const App = () => {
   }, [mode, autoplay])
 
   return (
-    <div>
+    <div className='page-container'>
       <div>
         <FlapDisplay
           className={`demoFlapper ${theme} ${colorScheme}`}
@@ -96,104 +96,102 @@ export const App = () => {
       <form onSubmit={(e) => e.preventDefault()}>
         <div className='row'>
           <div className='full'><h2>Demo mode</h2></div>
-          <div className='col'>
-            <label>
-              <input type='radio' name='mode' value={Modes.Numeric} checked={mode === Modes.Numeric} onChange={() => setMode(Modes.Numeric)} />
-              Numeric
-            </label>
-            <label>
-              <input type='radio' name='mode' value={Modes.Alphanumeric} checked={mode === Modes.Alphanumeric} onChange={() => setMode(Modes.Alphanumeric)} />
-              Alphanumeric
-            </label>
-            <label>
-              <input type='radio' name='mode' value={Modes.Words} checked={mode === Modes.Words} onChange={() => setMode(Modes.Words)} />
-              Words
-            </label>
+          <div className='full col-md-50'>
+            <input type='radio' name='mode' id='mode:numeric' value={Modes.Numeric} checked={mode === Modes.Numeric} onChange={() => setMode(Modes.Numeric)} />
+            <label for='mode:numeric'>Numeric</label>
+            <input type='radio' name='mode' id='mode:alpha' value={Modes.Alphanumeric} checked={mode === Modes.Alphanumeric} onChange={() => setMode(Modes.Alphanumeric)} />
+            <label for='mode:alpha'>Alphanumeric</label>
+            <input type='radio' name='mode' id='mode:words' value={Modes.Words} checked={mode === Modes.Words} onChange={() => setMode(Modes.Words)} />
+            <label for='mode:words'>Words</label>
           </div>
-          <div className='col'>
-            <label>
-              <input type='checkbox' name='autoplay' checked={autoplay} onChange={e => setAutoplay(e.target.checked)} /> Update automatically
-            </label>
+          <div className='full col-md-50'>
+            <input type='checkbox' id='autoplay' name='autoplay' checked={autoplay} onChange={e => setAutoplay(e.target.checked)} />
+            <label for='autoplay'>Update automatically</label>
           </div>
         </div>
         <div className='row'>
           <div className='full'><h2>CSS</h2></div>
-          <div className='col'>
-            <label>
-              Theme:
-              <select name='theme' value={theme} onChange={(e) => setTheme(e.target.value)}>
-                <option value=''>Default</option>
-                <option value='S'>S</option>
-                <option value='M'>M</option>
-                <option value='L'>L</option>
-                <option value='XL'>XL</option>
-              </select>
-            </label>
+          <div className='full col-md-50'>
+            <label for='theme'>Theme</label>
+            <select id='theme' name='theme' value={theme} onChange={(e) => setTheme(e.target.value)}>
+              <option value=''>Default</option>
+              <option value='S'>S</option>
+              <option value='M'>M</option>
+              <option value='L'>L</option>
+              <option value='XL'>XL</option>
+            </select>
           </div>
-          <div className='col'>
-            <label>
-              Color scheme:
-              <select name='colorScheme' value={colorScheme} onChange={(e) => setColorScheme(e.target.value)}>
-                <option value=''>Default</option>
-                <option value='light'>Light</option>
-                <option value='dark'>Dark</option>
-                <option value='lightBordered'>Light Bordered</option>
-                <option value='darkBordered'>Dark Bordered</option>
-              </select>
-            </label>
+          <div className='full col-md-50'>
+            <label for='colorScheme'>Color scheme</label>
+            <select id='colorScheme' name='colorScheme' value={colorScheme} onChange={(e) => setColorScheme(e.target.value)}>
+              <option value=''>Default</option>
+              <option value='light'>Light</option>
+              <option value='dark'>Dark</option>
+              <option value='lightBordered'>Light Bordered</option>
+              <option value='darkBordered'>Dark Bordered</option>
+            </select>
           </div>
         </div>
         <div className='row'>
-          <div className='full'><h2>Options</h2></div>
-          <div className='full'>
-            <label>
-              Class name <input type='text' name='className' disabled value={`${theme} ${colorScheme}`} />
-            </label>
+          <div className='full'><h2>Props</h2></div>
+          <div className='row'>
+            <label className='col25'><code>className</code></label>
+            <div className='col75'>
+              <input type='text' name='className' disabled value={`${theme} ${colorScheme}`} />
+            </div>
           </div>
-          <div className='full'>
-            <label>
-              Value <input type='text' name='value' value={value} disabled={autoplay} onChange={e => setValue(e.target.value)} />
-            </label>
+          <div className='row'>
+            <label className='col25'><code>value</code></label>
+            <div className='col75'>
+              <input type='text' name='value' value={value} disabled={autoplay} onChange={e => setValue(e.target.value)} />
+            </div>
           </div>
-          <div className='full'>
-            <label>
-              Chars <input type='text' name='chars' value={chars} disabled={mode === Modes.Words} onChange={e => setChars(e.target.value)} />
-            </label>
+          <div className='row'>
+            <label className='col25'><code>chars</code></label>
+            <div className='col75'>
+              <input type='text' name='chars' value={chars} disabled={mode === Modes.Words} onChange={e => setChars(e.target.value)} />
+            </div>
           </div>
-          <div className='full'>
-            <label>
-              Words <input type='text' name='words' value={Array(words).join(', ')} disabled={mode !== Modes.Words} onChange={e => setWords(String(e.target.value).split(/\s*,\s*/))} />
-            </label>
+          <div className='row'>
+            <label className='col25'><code>words</code></label>
+            <div className='col75'>
+              <input type='text' name='words' value={JSON.stringify(words)} disabled={mode !== Modes.Words} onChange={e => setWords(JSON.parse(e.target.value))} />
+            </div>
           </div>
-          <div className='full'>
-            <label>
-              Length <input type='number' name='length' value={length} min='1' disabled={mode === Modes.Words} onChange={e => setLength(Math.max(Number(e.target.value), 1))} />
-            </label>
+          <div className='row'>
+            <label className='col25'><code>length</code></label>
+            <div className='col75'>
+              <input type='number' name='length' value={length} min='1' disabled={mode === Modes.Words} onChange={e => setLength(Math.max(Number(e.target.value), 1))} />
+            </div>
           </div>
-          <div className='full'>
-            <label>
-              Timing <input type='number' name='timing' value={timing} min='1' onChange={e => setTiming(Math.max(Number(e.target.value), 1))} />
-            </label>
+          <div className='row'>
+            <label className='col25'><code>timing</code></label>
+            <div className='col75'>
+              <input type='number' name='timing' value={timing} min='1' onChange={e => setTiming(Math.max(Number(e.target.value), 1))} />
+            </div>
           </div>
-          <div className='full'>
-            <label>
-              Padding <input type='text' name='padding' value={padding} size='1' disabled={mode === Modes.Words} onChange={e => setPadding(String(e.target.value).slice(0, 1))} />
-            </label>
+          <div className='row'>
+            <label className='col25'><code>padding</code></label>
+            <div className='col75'>
+              <input type='text' name='padding' value={padding} size='1' disabled={mode === Modes.Words} onChange={e => setPadding(String(e.target.value).slice(0, 1))} />
+            </div>
           </div>
-          <div className='full'>
-            <label>
-              Pad mode:
+          <div className='row'>
+            <label className='col25'><code>padMode</code></label>
+            <div className='col75'>
               <select name='padMode' value={padMode} disabled={mode === Modes.Words} onChange={(e) => setPadMode(e.target.value)}>
                 <option value='auto'>Automatic</option>
                 <option value='start'>Start (right align)</option>
                 <option value='end'>End (left align)</option>
               </select>
-            </label>
+            </div>
           </div>
-          <div className='full'>
-            <label>
-              <input type='checkbox' name='hinge' checked={hinge} onChange={e => setHinge(e.target.checked)} /> Hinge
-            </label>
+          <div className='row'>
+            <label className='col25'><code>hinge</code></label>
+            <div className='col75'>
+              <input type='checkbox' id='hinge' name='hinge' checked={hinge} onChange={e => setHinge(e.target.checked)} />
+              <label for='hinge'>Show hinge</label>
+            </div>
           </div>
         </div>
       </form>
